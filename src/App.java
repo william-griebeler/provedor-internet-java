40b5bbb066210391869fd27421d23bc7297363e1
william griebeler <williamgriebeler10@gmail.com>
2024-04-04 21:55:37 -0300

    primeiro commit

---
+++ src/App.java
@@ -0,0 +1,13 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+    System.out.println("Olá, bem-vindo a central de atendimento {provedor}");
+
+    System.out.println("Meu nome é {atendente}, sou seu assistente virtual");
+
+    System.out.println("Durante o atendimento siga minhas instruções");
+
+    System.out.println("Primeiramente informe seu nome");
+
+    System.out.println("Obrigado {nome}, como posso lhe ajudar?");
+    }
+}
